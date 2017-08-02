c1ae444a05b86c6d7de97b91e54a9b357cb10cf8
Devan Agrawal <daagrawal@berkeley.edu>
2017-08-02 10:36:00 -0700

    expanded pt_hat range of event generator

--- src/devan/jetfinding.sh
+++ src/devan/jetfinding.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-jettyDevanExe --jetfinding --output="jetfinding_pthat10.root" Beams:eCM=2760. Main:numberOfEvents=1000 PhaseSpace:pTHatMin=10 PhaseSpace:pTHatMax=25
-jettyDevanExe --jetfinding --output="jetfinding_pthat25.root" Beams:eCM=2760. Main:numberOfEvents=1000 PhaseSpace:pTHatMin=25 PhaseSpace:pTHatMax=50
-jettyDevanExe --jetfinding --output="jetfinding_pthat50.root" Beams:eCM=2760. Main:numberOfEvents=1000 PhaseSpace:pTHatMin=50 PhaseSpace:pTHatMax=100
-jettyDevanExe --jetfinding --output="jetfinding_pthat100.root" Beams:eCM=2760. Main:numberOfEvents=1000 PhaseSpace:pTHatMin=100 PhaseSpace:pTHatMax=200
-jettyDevanExe --jetfinding --output="jetfinding_pthat200.root" Beams:eCM=2760. Main:numberOfEvents=1000 PhaseSpace:pTHatMin=200 PhaseSpace:pTHatMax=-1
+jettyDevanExe --jetfinding --output="jetfinding_pthat20.root" Beams:eCM=2760. Main:numberOfEvents=1000 PhaseSpace:pTHatMin=20 PhaseSpace:pTHatMax=25
+jettyDevanExe --jetfinding --output="jetfinding_pthat25.root" Beams:eCM=2760. Main:numberOfEvents=1000 PhaseSpace:pTHatMin=25 PhaseSpace:pTHatMax=30
+jettyDevanExe --jetfinding --output="jetfinding_pthat30.root" Beams:eCM=2760. Main:numberOfEvents=1000 PhaseSpace:pTHatMin=30 PhaseSpace:pTHatMax=35
+jettyDevanExe --jetfinding --output="jetfinding_pthat40.root" Beams:eCM=2760. Main:numberOfEvents=1000 PhaseSpace:pTHatMin=40 PhaseSpace:pTHatMax=50
+jettyDevanExe --jetfinding --output="jetfinding_pthat50.root" Beams:eCM=2760. Main:numberOfEvents=1000 PhaseSpace:pTHatMin=50 PhaseSpace:pTHatMax=60
+jettyDevanExe --jetfinding --output="jetfinding_pthat60.root" Beams:eCM=2760. Main:numberOfEvents=1000 PhaseSpace:pTHatMin=60 PhaseSpace:pTHatMax=70
+jettyDevanExe --jetfinding --output="jetfinding_pthat70.root" Beams:eCM=2760. Main:numberOfEvents=1000 PhaseSpace:pTHatMin=70 PhaseSpace:pTHatMax=85
+jettyDevanExe --jetfinding --output="jetfinding_pthat85.root" Beams:eCM=2760. Main:numberOfEvents=1000 PhaseSpace:pTHatMin=85 PhaseSpace:pTHatMax=105
+jettyDevanExe --jetfinding --output="jetfinding_pthat105.root" Beams:eCM=2760. Main:numberOfEvents=1000 PhaseSpace:pTHatMin=105 PhaseSpace:pTHatMax=-1
