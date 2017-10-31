349a3321c04ef69b360052f285e9e9dd2b9a08e1
Devan Agrawal <daagrawal@berkeley.edu>
2017-10-31 14:02:37 -0700

    fixed path to pythia.cmnd

--- src/devan/background.sh
+++ src/devan/background.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-jettyDevanExe --background --output="background.root" --config="~/lbnl/jetty/config/" Beams:eCM=13000. Main:numberOfEvents=1000 PhaseSpace:pTHatMin=90 PhaseSpace:pTHatMax=110
+jettyDevanExe --background --output="background.root" --config="~/lbnl/jetty/config/pythia.cmnd" Beams:eCM=13000. Main:numberOfEvents=1000 PhaseSpace:pTHatMin=90 PhaseSpace:pTHatMax=110
