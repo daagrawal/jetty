9125ce30f9b1d6d696b1f742272cd2b251575198
Devan Agrawal <daagrawal@berkeley.edu>
2017-11-28 13:22:27 -0800

    running 2000 background particles with pt 1.4 GeV

--- src/devan/run_background.cxx
+++ src/devan/run_background.cxx
@@ -49,7 +49,7 @@ int run_background (const std::string &s)
 		// this is where the event loop section starts
         auto nEv = args.getI("Main:numberOfEvents");
         LoopUtil::TPbar pbar(nEv);
-        double num_background_particles = 1000.;
+        double num_background_particles = 2000.;
         double sqrt_num_background = TMath::Sqrt(num_background_particles);
         double pt_background = 1400.;
         for (unsigned int iE = 0; iE < nEv; iE++)
